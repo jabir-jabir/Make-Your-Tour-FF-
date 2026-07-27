16aca5b1c43055dd7ffc67ff0c029579c80215de
jabir-jabir <elumilmu@gmail.com>
2026-07-27 16:25:42 +0600

    Create Banner.js

---
+++ models/Banner.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+const bannerSchema = new mongoose.Schema({
+    imageUrl: { type: String, required: true }
+});
+module.exports = mongoose.model('Banner', bannerSchema);
